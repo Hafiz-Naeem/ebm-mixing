f296376827ae2210ef7a677b44ce70c7559729b1
Hafiz-Naeem <naeemkhawaja25@gmail.com>
2018-09-13 12:22:30 +0500

    initial

---
+++ view/mainPage.controller.js
@@ -0,0 +1,22 @@
+
+
+sap.ui.controller("com.tallymarks.z_fiori_test.view.mainPage", {
+
+    onInit: function() {
+ 
+    //   debugger;
+
+    
+    },
+
+    // onCheckBoxSelect: function (oEvent) {
+    //     debugger;
+    //     var bFixedLayout = oEvent.getParameter("selected");
+    //     var oTable = oEvent.getSource().getParent().getParent();
+    //     oTable.setFixedLayout(bFixedLayout);
+    // },
+
+    
+    
+    
+  });
